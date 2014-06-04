2e69422d340b7a5dd6794d4d2b6fb2471dcc3515
Artur Kovalevsky <kovalevskyproj@gmail.com>
2014-06-04 10:46:20 +0300

    Parser moved to main() function

--- src/main.c
+++ src/main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "headers/loader.h"
-#include "headers/parser.h"
+//~ #include "headers/parser.h"
 
 int main()
 {   
-    char *data = getContent("text.txt"), *words;
-    int i;
+    char *data = getContent("text.txt"), *token, words[255][255];
+    int i = 0, j = 0;
     
     if (NULL == data) {
         return 0;
     }
+        
+    token = strtok(data, " ,.");
     
+    while (token != NULL) 
+    {
+		strcpy(words[j], token);
+        token = strtok(NULL, " ,.");
+		++j;
+    }
     
-    parse(data);
+	for (i = 0; i < j; ++i) {
+		printf("%s ", words[i]);
+	}
+	
+	printf("\n\n");
     
     return 1;
 }
