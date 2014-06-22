d374991d749d952c28d2daa2dc436e794ffabe2b
Artur Kovalevsky <kovalevskyproj@gmail.com>
2014-06-22 23:19:16 +0300

    Removes all non alphanumeric characters from the word

--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,58 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <algorithm>
+#include <locale>
+#include <ctype.h>
 #include <string.h>
+
 
 using namespace std;
 
+bool is_not_alphanum(char c)
+{
+    if (!isalnum((int)c)) {
+        return true;
+    }
+    
+    return false;
+}
+
 int main()
 {
-	string filepath, word;
-	char path[1024];
-	fstream file;
-	vector <string> words;
-	
-	cout << "Укажите путь к файлу:" << endl;
-	cin >> filepath;
-	
-	if (filepath.empty()) {
-		cout << "Путь к файлу не указан.";
-		return 0;
-	}
-	
-	strncpy(path, filepath.c_str(), sizeof(path));
-	path[sizeof(path) - 1] = 0;
-	
-	file.open(path, fstream::in);
-	if (!file.is_open()) {
-		cout << "Не удалось открыть файл: " << filepath << endl;
-		return 0;
-	}
-	
-	while (!file.eof()) {
-		file >> word;
-		words.push_back(word);
-	}
-	
-	for (int i = 0; i < (int)words.size(); i++) {
-		cout << words.at(i) << endl;
-	}
-	
-	file.close();
-	
-	return 1;
+    string filepath, word;
+    char path[1024];
+    fstream file;
+    vector <string> words;
+    
+    cout << "Укажите путь к файлу: ";
+    cin >> filepath;
+    
+    if (filepath.empty()) {
+        cout << "Путь к файлу не указан." << endl;
+        return 0;
+    }
+    
+    strncpy(path, filepath.c_str(), sizeof(path));
+    path[sizeof(path) - 1] = 0;
+    
+    file.open(path, fstream::in);
+    if (!file.is_open()) {
+        cout << "Не удалось открыть файл: " << filepath << endl;
+        return 0;
+    }
+    
+    while (!file.eof()) {
+        file >> word;
+        word.erase(remove_if(word.begin(), word.end(), is_not_alphanum), word.end());
+        words.push_back(word);
+    }
+    
+    for (int i = 0; i < (int)words.size(); i++) {
+        cout << words.at(i) << endl;
+    }
+    
+    file.close();
+    
+    return 1;
 }
